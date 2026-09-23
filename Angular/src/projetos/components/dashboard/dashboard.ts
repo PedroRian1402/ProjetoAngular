7c1d5ea257a872a803d30d3e1462375da623f860
PedroRian02 <pedrorian02@gmail.com>
2026-09-23 18:34:26 +0100

    Formulário
    Criação de um formulário para a criação de novos projetos.

--- Angular/src/projetos/components/dashboard/dashboard.ts
+++ Angular/src/projetos/components/dashboard/dashboard.ts
@@ -2,17 +2,26 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ProjectCard } from '../project-card/project-card';
 import { Project, CreateProject } from '../../types/project.types';
+import { ProjectForm } from '../project-form/project-form';
 
 @Component({
-  imports: [CommonModule, ProjectCard],
+  imports: [CommonModule, ProjectCard, ProjectForm],
   selector: 'app-dashboard',
   styleUrl: './dashboard.css',
   templateUrl: './dashboard.html',
 })
 export class Dashboard {
   projects: Project[] = [];
+  isModalOpen = false
 
-  openCreateModal(): void {
-    alert('Abrir formulário de criação de projeto!');
+  handleCreateProject(newProjectData: CreateProject): void {
+    const newProject: Project = {
+      id : Math.random().toString(36).substring(2, 9),
+      name : newProjectData.name,
+      description : newProjectData.description,
+      task: []
+    }
+    this.projects = [newProject, ...this.projects]
+    this.isModalOpen = false
   }
 }
--- Angular/src/projetos/components/project-form/project-form.ts
+++ Angular/src/projetos/components/project-form/project-form.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
+import { CreateProject } from '../../types/project.types'
 
 @Component({
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   selector: 'app-project-form',
   styleUrl: './project-form.css',
   templateUrl: './project-form.html',
 })
-export class ProjectForm {}
+
+export class ProjectForm {
+  @Output() submitSuccess = new EventEmitter<CreateProject>()
+  @Output() cancel = new EventEmitter<void>()
+
+  projectForm = new FormGroup({
+    name: new FormControl('', { validators : [Validators.required, Validators.minLength(3)], nonNullable: true}),
+    description: new FormControl('', { validators: [Validators.required, Validators.minLength(4)], nonNullable: true})
+  })
+
+  onSubmit(): void {
+    if(this.projectForm.valid){
+      const formValue: CreateProject = this.projectForm.getRawValue()
+      this.submitSuccess.emit(formValue)
+    } else {
+      this.projectForm.markAllAsTouched()
+    }
+  }
+}
