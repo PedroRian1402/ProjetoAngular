5d15f1aa847ceccc8b8237b57aa5c16a478c7d0f
PedroRian02 <pedrorian02@gmail.com>
2026-09-23 13:57:01 +0100

    Tipagem de Projetos e tarefas
    Criação de uma pasta, com ficheiro .ts para garantir a tipagem das interfaces do Projetos, tarefas e de uma interface para a validação do formulario enviado.

---
+++ Angular/src/projetos/types/project.types.ts
@@ -0,0 +1,17 @@
+export interface Project {
+    id : string;
+    name : string;
+    description: string
+    task: Task[]
+}
+
+export interface Task{
+    id: string;
+    title: string;
+    isCompleted: boolean;
+}
+
+export interface CreateProject{
+    name: string;
+    description: string
+}
