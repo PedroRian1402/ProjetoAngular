48372f2a67020de6cb28fa515b07bce55f1cc50c
PedroRian02 <pedrorian02@gmail.com>
2026-09-24 15:41:27 +0100

    Aperfeiçoamento da APIRestful
    Aprimoranto e correção de bugs gerados da criação original da API,
    também foi criado uma medida para iniciar o servidor do angular e do API com apenas um codigo.

--- Angular/src/projetos/components/dashboard/dashboard.ts
+++ Angular/src/projetos/components/dashboard/dashboard.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { ProjectCard } from '../project-card/project-card';
 import { Project, CreateProject } from '../../types/project.types';
+import { ProjectService } from '../../services/project';
 import { ProjectForm } from '../project-form/project-form';
 
 @Component({
@@ -11,17 +12,42 @@ import { ProjectForm } from '../project-form/project-form';
   templateUrl: './dashboard.html',
 })
 export class Dashboard {
+
+  private projectService = inject(ProjectService)
+  private cdr = inject(ChangeDetectorRef)
   projects: Project[] = [];
   isModalOpen = false
 
+  constructor(){
+    this.loadProjects()
+  }
+
+  private loadProjects():void {
+    this.projectService.getProjects().subscribe({
+      next: (data) =>{
+        this.projects = [...data]
+        this.cdr.detectChanges()
+      },
+      error : (err)=>{
+        console.error('Erro ao ler a API:', err)
+      }
+    })
+  }
+
   handleCreateProject(newProjectData: CreateProject): void {
-    const newProject: Project = {
-      id : Math.random().toString(36).substring(2, 9),
-      name : newProjectData.name,
-      description : newProjectData.description,
-      task: []
-    }
-    this.projects = [newProject, ...this.projects]
+    this.projectService.createProject(newProjectData).subscribe({
+      next : (newProjectCreatedInServe) => {
+        this.projects = [newProjectCreatedInServe, ...this.projects]
+        this.isModalOpen = false
+        this.cdr.detectChanges()
+      },
+      error: (err) =>{
+        console.error('Error ao gravar na API:', err)
+      }
+    })
+  }
+
+  closeModal(): void {
     this.isModalOpen = false
   }
 }
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Instalei uma ferramenta chamada concurrently no terminal para poder iniciar o servidor do angular e do API ao mesmo tempo.
+
+Para iniciar o servidor local, basta abrir o terminal certificar-se que esta na pasta Angular dentro de Projeto Angular, e escrever no terminal o codigo abaixo:
+
+npm run dev
+
+O concurrently vai abrir o servidor do Angular na porta padrão (http://localhost:4200) e a API simulada na porta 3000 em simultâneo.
